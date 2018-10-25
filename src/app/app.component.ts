5ca3cd139d8d10409abf730ce245d51a937aea9c
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-10-25 22:19:49 +0530

    parse Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   private fileText;
+  private data;
   title = 'jsontochart';
+
   fileUpload(event) {
     var reader = new FileReader();
     reader.readAsText(event.srcElement.files[0]);
+
     var me = this;
+
     reader.onload = function () {
+      me.data = JSON.parse(reader.result);
+      console.log(me.data);
       me.fileText = reader.result;
     }
   }
