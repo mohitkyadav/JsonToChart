a82e285e8820544d7ccce7835378c0641bcc4e66
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-10-25 23:19:41 +0530

    display charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  private fileText;
-  private data;
+  chart = [];
+  fileText;
+  data;
   title = 'jsontochart';
 
   fileUpload(event) {
@@ -21,6 +23,26 @@ export class AppComponent {
       me.data = JSON.parse(reader.result);
       console.log(me.data);
       me.fileText = reader.result;
+      me.chart = new Chart('canvas', {
+        type: 'line',
+        data: {
+          labels: me.data.groups[0].peaks[0].eic.rt,
+          datasets: [
+            {
+              label: me.data.groups[0].peaks[0].sampleName,
+              data: me.data.groups[0].peaks[0].eic.intensity,
+              borderColor: '#b9090b',
+              fill: true
+            },
+            {
+              label: me.data.groups[0].peaks[1].sampleName,
+              data: me.data.groups[0].peaks[1].eic.intensity,
+              borderColor: '#000080',
+              fill: true
+            },
+          ]
+        }
+      });
     }
   }
 }
