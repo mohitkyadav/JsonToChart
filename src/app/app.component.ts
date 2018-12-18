c4995a8cf41275c9e422c3f7e497ca39cd10b072
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-12-18 12:50:34 +0530

    Update readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,49 +23,11 @@ export class AppComponent {
       me.data = JSON.parse(reader.result);
       console.log(me.data);
       me.fileText = reader.result;
+
       me.chart = new Chart('canvas', {
-        type: 'line',
-        data: {
-          labels: me.data.groups[1].peaks[0].eic.rt,
-          datasets: [
-            {
-              label: me.data.groups[1].peaks[0].sampleName,
-              data: me.data.groups[1].peaks[0].eic.intensity,
-              borderColor: '#008000',
-              fill: true
-            },
-            {
-              label: me.data.groups[1].peaks[1].sampleName,
-              data: me.data.groups[1].peaks[1].eic.intensity,
-              borderColor: '#800080',
-              fill: true
-            },
-            {
-              label: me.data.groups[1].peaks[2].sampleName,
-              data: me.data.groups[1].peaks[2].eic.intensity,
-              borderColor: '#FFD700',
-              fill: true
-            },
-
-          ]
-        },
-        options: {
-          scales: {
-            xAxes: [{
-              scaleLabel: {
-                display: true,
-                labelString: 'rt'
-              }
-            }],
-
-            yAxes: [{
-              scaleLabel: {
-                display: true,
-                labelString: 'intensity'
-              }
-            }]
-          }
-        }
+        type: me.data.meta.type,
+        data: me.data.data,
+        options: me.data.options
       });
     }
   }
